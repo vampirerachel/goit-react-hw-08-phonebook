3220afe15d43f5ae73f0728cc2e1299d07a41125
Rachel Frederickson <vampirerachel@gmail.com>
2023-07-07 18:05:49 -0400

    package.json update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import SignIn from "./SignIn";
-import SignUp from "./SignUp";
 import Header from "./Header";
 import { Routes, Route } from "react-router-dom";
 
@@ -10,7 +9,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<SignIn />} />
-        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/SignIn" element={<SignIn />} />
       </Routes>
     </div>
   );
